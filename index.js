10963fd0e881e58adb40312c1cf14cd944206eab
gnardinosaur <github email address>
2019-11-16 12:15:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here
+const var1 = 62;
+const var2 = 1;
+
+const multiply = var1 * var2;
+
+const random = Math.random() * 10;
+
+const mod = 24 % 5;
+
+const max = Math.max(1, 3, 20);
+
