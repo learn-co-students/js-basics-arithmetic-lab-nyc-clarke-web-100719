1257ac88a6720f0a16b7efe9d0f4f33008280fdf
GeneHFang <ghl234@nyu.edu>
2019-11-17 17:52:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here
+
+var var1 = 31;
+var var2 = 2;
+
+var multiply = var1*var2;
+
+var random = Math.floor(Math.random()*10) + 1;
+
+var mod = 14%10;
+var arr = [1,2,3,4,5,6,20];
+var max = Math.max.apply(Math,arr);
