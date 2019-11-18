5ea6ed1ffeba17a9452db62a1e112e65b92dec3a
Barbara <muesingb@gmail.com>
2019-11-18 11:32:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here
+const var1 = 31
+const var2 = 2
+const multiply = var1 * var2
+
+const random = Math.random() * 10
+
+const mod = 60%7
+
+const max = Math.max(1, 5, 20, 14, 16, 8)
