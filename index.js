4a1a13b2e0632f4456c8d9d4c0160009880f7f36
jastaffen <jack.staffen@gmail.com>
2019-11-18 00:15:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here
+const var1 = 31;
+const var2 = 2;
+
+const multiply = var1 * var2;
+
+const random = Math.random() + 1;
+
+const mod = 9 % 5;
+
+const max = Math.max(1, 10, 20);
